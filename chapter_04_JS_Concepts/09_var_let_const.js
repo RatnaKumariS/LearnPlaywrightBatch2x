573f5c42d8038a84dda7ef2acfed0d2ac5b8dad2
RatnaKumariS <ratnakumari@example.com>
2026-05-25 00:11:17 +0530

    Add chapter_04_JS_Concepts with var/let/const examples

---
+++ chapter_04_JS_Concepts/09_var_let_const.js
@@ -0,0 +1,33 @@
+// var v = 10;
+// let l = 8;
+// const c = 5.1;
+
+
+// let number = 7;
+// if (number % 2 === 0) {
+//     console.log("even" + number);
+// } else {
+//     console.log("odd:   " + number);
+// }
+
+// let grade = 79;
+
+// if (grade > 90) {
+//     console.log("Grade A");
+// } else if (grade > 80) {
+//     console.log("Grade B");
+// } else if (grade > 70) {
+//     console.log("Grade C");
+// } else if (grade > 60) {
+//     console.log("Grade D");
+// } else {
+//     console.log("Fail");
+// }
+
+
+let year = 2024;
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    console.log("Leap year");
+} else if (year > 80) {
+    console.log("Not Leap year");
+}
