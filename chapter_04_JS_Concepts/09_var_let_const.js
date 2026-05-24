f066eb727d670e50d96e16b97ee334db3ffbe4a0
RatnaKumariS <ratnakumari@example.com>
2026-05-25 00:13:07 +0530

    Uncomment and activate JS examples in var_let_const

--- chapter_04_JS_Concepts/09_var_let_const.js
+++ chapter_04_JS_Concepts/09_var_let_const.js
@@ -1,28 +1,28 @@
-// var v = 10;
-// let l = 8;
-// const c = 5.1;
+var v = 10;
+let l = 8;
+const c = 5.1;
 
 
-// let number = 7;
-// if (number % 2 === 0) {
-//     console.log("even" + number);
-// } else {
-//     console.log("odd:   " + number);
-// }
+let number = 7;
+if (number % 2 === 0) {
+    console.log("even" + number);
+} else {
+    console.log("odd:   " + number);
+}
 
-// let grade = 79;
+let grade = 79;
 
-// if (grade > 90) {
-//     console.log("Grade A");
-// } else if (grade > 80) {
-//     console.log("Grade B");
-// } else if (grade > 70) {
-//     console.log("Grade C");
-// } else if (grade > 60) {
-//     console.log("Grade D");
-// } else {
-//     console.log("Fail");
-// }
+if (grade > 90) {
+    console.log("Grade A");
+} else if (grade > 80) {
+    console.log("Grade B");
+} else if (grade > 70) {
+    console.log("Grade C");
+} else if (grade > 60) {
+    console.log("Grade D");
+} else {
+    console.log("Fail");
+}
 
 
 let year = 2024;
